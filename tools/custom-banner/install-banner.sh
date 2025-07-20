1354957593e66a67ef41b243a46ea8bf29ce3968
Kydras8 <your-no-reply-email>
2025-07-19 19:06:19 -0600

    Fixed banner with proper files

---
+++ tools/custom-banner/install-banner.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+cat ~/Kydras-Termux-Pack/tools/custom-banner/kydras-banner.txt
+echo "GitHub User: Kydras8"
+echo "Welcome, Mr. K - Kydras Operator Mode is Live."
+echo ""
